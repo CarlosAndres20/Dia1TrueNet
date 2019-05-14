ea9b2c63112f722f9b34bd4586e7e87174aa1c20
Sebastian <>
2019-05-14 17:40:47 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dia1TrueNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
